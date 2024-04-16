2482c4a5ea756f671a09e582088595b7dd6b93aa
Benjamin Miller <benmiller179@gmail.com>
2024-04-16 18:35:55 -0500

    Update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,8 @@ import { resolve } from 'path';
 export default defineNuxtConfig({
   devtools: { enabled: true },
   alias: {
-    '@': resolve(__dirname, '/')
+    '@': resolve(__dirname, '/'),
+    assets: '/<rootDir>/assets'
   },
   css: ['~/assets/main.scss'],
   postcss: {
