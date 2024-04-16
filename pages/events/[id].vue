6e88ccc64bc96235aecb758ec7c7dbdfdec3e1bc
Benjamin Miller <benmiller179@gmail.com>
2024-04-16 11:48:44 -0500

    Update

--- pages/events/[id].vue
+++ pages/events/[id].vue
@@ -1,3 +1,10 @@
+<script setup>
+const route = useRoute();
+console.log('id.vue:' + route.params.id);
+const nuxt = useNuxtApp();
+console.log(nuxt);
+</script>
+
 <template>
   <div>[id].vue: {{ $route.params.id }}</div>
 </template>
