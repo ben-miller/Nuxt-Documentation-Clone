52ced71782d997bb09d41bfe5c84c0940b83443d
Benjamin Miller <benmiller179@gmail.com>
2024-04-16 19:15:12 -0500

    Update

--- composables/useUtils.ts
+++ composables/useUtils.ts
@@ -1,6 +1,6 @@
-export const useUtils = () => {
-    const sayHello = () => console.log("Hello!");
-    return {
-        sayHello,
-    }
-}
+//export const useUtils = () => {
+//    const sayHello = () => console.log("Hello!");
+//    return {
+//        sayHello,
+//    }
+//}
