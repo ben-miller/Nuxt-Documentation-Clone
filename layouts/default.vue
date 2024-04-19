4c9239c933396404d06fe91b9ab32aba6290610f
Benjamin Miller <benmiller179@gmail.com>
2024-04-19 17:17:43 -0500

    Update

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,33 @@
+<script setup>
+const docsNavLinks = [
+  { id: 'get_started', text: 'Get Started', href: 'https://nuxt.com/docs/getting-started/introduction' },
+  { id: '', text: 'Guide', href: '' },
+  { id: '', text: 'API', href: '' },
+  { id: '', text: 'Examples', href: '' },
+  { id: '', text: 'Community', href: '' },
+  ];
+</script>
+
 <template>
   <div class="DefaultLayout text-gray-300" style="background-color: #020420">
     <NavBar />
     <main>
       <div class="mx-auto px-4 sm:px-6 lg:px-8 max-w-7x1">
         <div class="flex flex-col lg:grid lg:grid-cols-10 lg:gap-8">
-          <div class="lg:col-span-2">Left Nav</div>
+          <div class="lg:col-span-2">
+            <aside>
+              <li v-for="item in docsNavLinks">
+                {{ item.text }}
+              </li>
+            </aside>
+          </div>
           <div class="lg:col-span-8">
             <div class="flex flex-col lg:grid lg:grid-cols-10 lg:gap-8">
               <div class="lg:col-span-8">
                 <slot />
               </div>
               <div class="lg:col-span-2 order-first lg:order-last">
-                <div>Right Nav</div>
+                Right Nav
               </div>
             </div>
           </div>
