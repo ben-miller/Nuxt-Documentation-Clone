f415272eaf43abea6da152f7f57e709b5d3283c9
Benjamin Miller <benmiller179@gmail.com>
2024-04-22 20:15:19 -0500

    Update

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,20 +1,31 @@
 <template>
-  <header class="h-[--header-height] sticky top-0 z-50 backdrop-blur">
-    <ul>
-      <li>
-        <NuxtLink to="/">Nuxt</NuxtLink>
-        <NuxtLink to="/docs">Docs</NuxtLink>
-        <NuxtLink to="/integrations">Integrations</NuxtLink>
-        <NuxtLink to="/resources">Resources</NuxtLink>
-        <NuxtLink to="/showcase">Showcase</NuxtLink>
-        <NuxtLink to="/blog">Enterprise</NuxtLink>
-      </li>
-    </ul>
+  <header class="sticky top-0 z-50 backdrop-blur">
+    <div class="h-[--header-height] mx-auto px-4 lg:px-8 max-w-7xl flex items-center justify-between gap-3">
+      <div class="NuxtLogo">
+        Nuxt logo
+      </div>
+      <div class="HeaderLinks">
+        <ul>
+          <li>
+            <NuxtLink to="/">Nuxt<span class="i-ph-caret-down w-4 h-4" /></NuxtLink>
+            <NuxtLink to="/docs">Docs</NuxtLink>
+            <NuxtLink to="/integrations">Integrations</NuxtLink>
+            <NuxtLink to="/resources">Resources</NuxtLink>
+            <NuxtLink to="/showcase">Showcase</NuxtLink>
+            <NuxtLink to="/enterprise">Enterprise</NuxtLink>
+            <NuxtLink to="/blog">Blog</NuxtLink>
+          </li>
+        </ul>
+      </div>
+      <div class="UpperRightButtons">
+        Search, Dark Mode, GitHub
+      </div>
+    </div>
   </header>
 </template>
 
 <style scoped>
 header li a {
-  @apply px-3;
+  @apply px-3 text-sm/6 font-semibold;
 }
 </style>
